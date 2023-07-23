0f52d5a07c7e0c203979225ea56a6b9c9751a248
robfoss <robfoss@rocketmail.com>
2023-07-23 15:12:07 -0400

    added PostListItem component and types

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -18,16 +18,10 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+ 
 });
--- src/components/PostListItem.tsx
+++ src/components/PostListItem.tsx
@@ -1,20 +1,86 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import { FontAwesome } from '@expo/vector-icons'
 import { Post } from '@/types'
 
 type PostListItemProps = {
   post: Post
 }
 
+type FooterButtonProps = {
+  text: string,
+  icon: React.ComponentProps<typeof FontAwesome>['name']
+}
+
+function FooterButton({text, icon}: FooterButtonProps){
+  return (
+    <View style={{flexDirection: 'row'}}>
+      <FontAwesome name={icon} size={16} color="gray" />
+      <Text style={{marginLeft: 5, color: 'gray', fontWeight: '500'}}>{text}</Text>
+    </View>
+  )
+}
+
 
 const PostListItem = ({post}: PostListItemProps) => {
   return (
-    <View>
-      <Text>{post.content}</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={{uri: post.author.image}} style={styles.userImage} />
+        <View>
+          <Text style={styles.username}>{post.author.name}</Text>
+          <Text>{post.author.position}</Text>
+        </View>
+        
+      </View>
+      <Text style={styles.content}>{post.content}</Text>
+     { post.image && <Image source={{uri: post.image}} style={styles.postImage} />}
+     <View style={styles.footer}>
+      <FooterButton  text={'Like'} icon={'thumbs-o-up'}/>
+      <FooterButton text={'Comment'} icon={'comment-o'}/>
+      <FooterButton text={'Share'} icon={'share'}/>
+     </View>
     </View>
   )
 }
 
 export default PostListItem
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+
+  },
+  username: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    marginBottom: 5
+  },
+  userImage: {
+    width: 50, 
+    height: 50,
+    borderRadius: 25,
+    marginRight: 10
+  },
+  content: {
+    margin: 10,
+    marginTop: 0,
+  },
+  postImage: {
+    width: '100%', 
+    aspectRatio: 1
+  },
+  footer: {
+    flexDirection: 'row',
+    paddingVertical: 10,
+    justifyContent: 'space-around',
+    borderTopWidth: 1,
+    borderColor: 'lightgray'
+
+  }
+
+})
